6276eab18c1d710fd229afdede278a63564adbf0
TongZJ <1400721986@qq.com>
2024-02-17 02:20:43 +0800

    update on 2024-02-17 02:20:43.417847

--- docker/cpp-bin/install-all.bash
+++ docker/cpp-bin/install-all.bash
@@ -2,13 +2,14 @@
 
 install-glog.bash
 install-eigen.bash
+install-fmt.bash
+install-octomap.bash
+install-pcl.bash
+
 install-ceres.bash    # after eigen, glog
 install-opencv.bash
 install-dbow3.bash    # after opencv
-install-fmt.bash
 install-g2o.bash    # after eigen
-install-octomap.bash
 install-pangolin.bash
-install-pcl.bash
 install-ros.bash
 install-sophus.bash    # after eigen, fmt
