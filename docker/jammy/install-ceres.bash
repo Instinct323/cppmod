a7036266aad8881a233028ef7daf928f12891940
TongZJ <1400721986@qq.com>
2024-03-29 16:32:41 +0800

    update on 2024-03-29 16:32:41.169800

--- docker/jammy/install-ceres.bash
+++ docker/jammy/install-ceres.bash
@@ -3,5 +3,5 @@
 # REQUIRE eigen, glog
 export TMP=/tmp/ceres
 apt install -y libgoogle-glog-dev libgflags-dev libatlas-base-dev libsuitesparse-dev
-git clone -b 2.2.0 https://github.com/ceres-solver/ceres-solver $TMP
+git clone -b 2.0.0 https://github.com/ceres-solver/ceres-solver $TMP
 cmake-install.bash $TMP
