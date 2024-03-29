a081a6bd5c7cbda3cf2deee9e95cd245facd81cf
TongZJ <1400721986@qq.com>
2024-03-29 23:53:04 +0800

    update on 2024-03-29 23:53:03.766772

--- docker/jammy/install-all.bash
+++ docker/jammy/install-all.bash
@@ -6,7 +6,5 @@ install-eigen.bash
 install-octomap.bash
 install-pcl.bash
 
-install-ceres.bash    # CMAKE 3.16...3.27, REQUIRE eigen, glog
-install-sophus.bash    # REQUIRE eigen, fmt
 install-g2o.bash    # REQUIRE eigen
 install-opencv.bash   # CMAKE 3.5.1
