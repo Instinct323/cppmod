b4cf453544b81da6bc66264670dbd5b5cd4c435f
TongZJ <1400721986@qq.com>
2024-05-02 20:41:21 +0800

    update on 2024-05-02 20:41:21.144510

--- main.cpp
+++ main.cpp
@@ -13,11 +13,8 @@ int main(int argc, char **argv) {
   LOG(INFO) << "CXX standard: " << __cplusplus;
 
   std::cout.precision(6);
-  camera::KannalaBrandt8 cam({190.97847715128717, 190.9733070521226, 254.93170605935475, 256.8974428996504,
-                              0.0034823894022493434, 0.0007150348452162257, -0.0020532361418706202, 0.00020293673591811182});
-  for (float x = 0; x < 640; x++) {
-    for (float y = 0; y < 480; y++) cam.unproject({x, y});
-  }
+  cv::remap();
+  cv::initUndistortRectifyMap();
 
   return 0;
 }
