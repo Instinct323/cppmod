bfd31cd9b130d6c4a948796869829c55a0fed7ea
TongZJ <1400721986@qq.com>
2024-09-14 22:06:18 +0800

    update on 2024-09-14 22:06:18.345087

--- docker/bin/clone-bin.bash
+++ docker/bin/clone-bin.bash
@@ -4,6 +4,7 @@ export TMP=/tmp/cppmod
 export BIN=/usr/local/bin
 
 git clone https://github.com/Instinct323/cppmod $TMP
+cp $TMP/docker/bin/* $BIN/
 cp $TMP/docker/$(lsb_release -cs)/*.bash $BIN/
 chmod +x $BIN/*.bash
 rm -rf $TMP
