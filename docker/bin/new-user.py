71ed83d14f9a2764bafcfbd9275847b07fd7bfc4
tongzj <tongzanjia@qq.com>
2024-12-05 10:40:47 +0800

    update

--- docker/bin/new-user.py
+++ docker/bin/new-user.py
@@ -6,8 +6,9 @@ import os
 
 
 def execute(cmd, check=True):
-    ret = print(cmd) or os.system(cmd)
+    ret = print("\033[32m\033[1m" + cmd + "\033[0m") or os.system(cmd)
     if check and ret: raise OSError(f"Fail to execute: {cmd}")
+    return ret
 
 
 class UserCfg(dict):
@@ -17,16 +18,20 @@ class UserCfg(dict):
         with open(file, "r") as f:
             super().__init__(json.load(f))
 
-    def execute(self):
-        execute(f"useradd -m {self['name']}")
-        execute(f"echo {self['name']}:{self['passwd']} | chpasswd")
+    def generate(self):
+        # Linux
+        if os.name == "posix":
+            execute(f"useradd -m {self['name']}")
+            execute(f"echo {self['name']}:{self['passwd']} | chpasswd")
         # git
+        execute("git config --global credential.helper store")
         execute(f"git config --global user.name {self['name']}")
         execute(f"git config --global user.email {self['email']}")
+        execute(f"ssh-keygen -t rsa -C {self['email']} -N ''")
 
 
 if __name__ == '__main__':
-    print("WARNING: This script should be executed in the container!")
+    print("\033[33m\033[1m" + "WARNING: This script should be executed in the container!" + "\033[0m")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("json", type=str, help="Docker container configuration file")
@@ -34,4 +39,4 @@ if __name__ == '__main__':
 
     # python3 bin\new-user.py user-tongzj.json
     ucfg = UserCfg(args.json)
-    ucfg.execute()
+    ucfg.generate()
