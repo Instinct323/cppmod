8aff0274b6c124a7898844c759e3e7f3c5b40aae
tongzj <tongzanjia@qq.com>
2025-03-15 15:23:11 +0800

    update on 2025-03-15 15:23:11.323467

--- docker/focal/install-opencv.bash
+++ docker/focal/install-opencv.bash
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-export TMP=/tmp/opencv
-apt install -y libgtk2.0-dev libjpeg-dev libopenexr-dev libtbb-dev
-git clone https://github.com/opencv/opencv $TMP
-cmake-install.bash $TMP
-ln -s /usr/local/include/opencv4/opencv2 /usr/local/include
+if [ $(id -u) -eq 0 ]; then
+  export TMP=/tmp/opencv
+  apt install -y libgtk2.0-dev libjpeg-dev libopenexr-dev libtbb-dev
+
+  git clone https://github.com/opencv/opencv $TMP
+  git clone https://github.com/opencv/opencv_contrib $TMP_contrib
+
+  cd $TMP
+  mkdir build && cd build
+  cmake -DOPENCV_EXTRA_MODULES_PATH=$TMP_contrib/modules ..
+  make
+  make install
+
+  # cmake-install.bash $TMP
+  # ln -s /usr/local/include/opencv4/opencv2 /usr/local/include
+else
+  echo "error: permission denied."
+fi
