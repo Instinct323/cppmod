99a4a431c0cff5d79ff5c065c6dedd5170bb8864
tongzj <tongzanjia@qq.com>
2025-03-19 15:38:00 +0800

    update on 2025-03-19 15:38:00.141587

---
+++ docker/focal/install-ros-foxy.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+apt install -y software-properties-common
+add-apt-repository universe
+
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+apt update
+apt upgrade -y
+apt install -y ros-foxy-desktop python3-argcomplete
+
+# Python 3.8 Libraries
+export TMP=/usr/local/share/ros-foxy-py38
+mkdir -p $TMP
+cp -r /opt/ros/foxy/lib/python3.8/site-packages/* $TMP
+rm -rf $TMP/*.egg-info
--- docker/focal/install-ros-noetic.bash
+++ docker/focal/install-ros-noetic.bash
@@ -3,7 +3,9 @@
 # https://wiki.ros.org/noetic/Installation/Ubuntu
 echo "deb http://packages.ros.org/ros/ubuntu focal main" > /etc/apt/sources.list.d/ros-latest.list
 cat /tmp/ros.asc | apt-key add -
+
 apt update
+apt upgrade -y
 apt install -y ros-noetic-desktop-full
 
 # Python 3 Libraries
